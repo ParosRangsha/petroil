92fe6cee729b97d44e5d0ed969a6ce4af273d2f8
paros <paroscrangsha@gmail.com>
2024-12-15 09:14:55 +0600

    menu add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,56 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaPhoneAlt } from "react-icons/fa"
+import { MdMailOutline } from "react-icons/md";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import logo from '../img/logo.png'
+
 const Header = () => {
   return (
     <div className="header">
         <div className="subheader bg-[#282828] py-[15px]">
             <div className="contain max-w-container mx-auto flex justify-between">
                 <div className="contact flex gap-4">
-                    <div className="mail text-[white]"><i className="fa-regular fa-envelope"></i> mail@yourcompany.com</div>
-                    <div className="mail text-[white]"><i className="fa-solid fa-phone"></i> +896 120 5889 (Toll free)</div>
+                    <div className="mail text-[white] flex gap-1"><MdMailOutline /> mail@yourcompany.com</div>
+                    <div className="mail text-[white] flex gap-1"><FaPhoneAlt /> +896 120 5889 (Toll free)</div>
                 </div>
                 <div className="sociallink flex gap-4 text-[white]">
-                    <Link to='https://facebook.com/paroscrangsha'><i className="fa-brands fa-facebook-f"></i></Link>
-                    <Link to='/'><i className="fa-brands fa-twitter"></i></Link>
-                    <Link to='/'><i className="fa-brands fa-linkedin-in"></i></Link>
-                    <Link to='/'><i className="fa-brands fa-instagram"></i></Link>
+                    <Link to='https://facebook.com/paroscrangsha'><FaFacebookF /></Link>
+                    <Link to='/'><FaTwitter /></Link>
+                    <Link to='/'><FaLinkedinIn /></Link>
+                    <Link to='/'><FaInstagram /></Link>
+                </div>
+            </div>
+        </div>
+        <div className="nav bg-[#F40404] py-[15px]">
+            <div className="contain max-w-container mx-auto flex justify-between">
+                <div className="logo">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="menus">
+                    <ul className='flex align-middle justify-center text-[white]'>
+                        <li>
+                            <Link to='/' className='py-[12px] px-[24px] inline-block font-[700]'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/' className='py-[12px] px-[24px] inline-block font-[700]'>About</Link>
+                        </li>
+                        <li>
+                            <Link to='/' className='py-[12px] px-[24px] inline-block font-[700]'>Services</Link>
+                        </li>
+                        <li>
+                            <Link to='/' className='py-[12px] px-[24px] inline-block font-[700]'>Gallery</Link>
+                        </li>
+                        <li>
+                            <Link to='/' className='py-[12px] px-[24px] inline-block font-[700]'>Blog</Link>
+                        </li>
+                        <li>
+                            <Link to='/' className='py-[12px] px-[24px] inline-block font-[700] border-[2px] border-[white] rounded-[5px] transition duration-[1s] hover:bg-[yellow] '>Contact</Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
